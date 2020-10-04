a60804038168b0da6117726fe999b70fde545c4c
shiyanwy <70578283+shiyanwy@users.noreply.github.com>
2020-10-04 23:21:23 +0800

    abc

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
-import base64
-import binascii
+import requests
+import os
 
-with open(r'doc\stego.txt') as fobj:
-    lines = fobj.readlines()
+def get_pic_by_url(lists):
+    output_path = './output'
+    for url in lists:
+        filename = output_path + '/' + url.split('/')[-1]
+        if os.path.exists(filename):
+            print('File have already exist.')
+            continue
+        try:
+            r = requests.get(url,timeout=5)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            continue
+        with open(filename,'wb') as f:
+            f.write(r.content)
 
-for line in lines:
-    print(base64.b64decode(line).decode('iso-8859-1'))
+if __name__ == "__main__":
+    file_path = './urllist.txt'
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        url_list = []
+        for line in lines:
+            url_list.append(line.strip('\n'))
+    
+    get_pic_by_url(url_list)
