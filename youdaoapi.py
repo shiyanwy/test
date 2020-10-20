814473028966d37057070e67b4ba8eafe2f2d3c8
WangYong <shiyanwy@hotmail.com>
2020-10-20 17:06:28 +0800

    a

--- youdaoapi.py
+++ youdaoapi.py
@@ -1,31 +1,47 @@
-import requests
 import json
+import requests
 
-class Youdao():
-    def __init__(self,word):
-        self.url='http://fanyi.youdao.com/openapi.do'    #url、key、keyfrom都是固定的值，所以采用这种方式赋值
-        self.key='929705525'
-        self.keyfrom='pythonxiaodeng'
-        self.word=word
-        
-    def getTranslation(self):
-        data={'keyfrom':self.keyfrom,
-            'key':self.key,
-            'type':'data',
-            'doctype':'json',
-            'version':'1.1',
-            'q':self.word}
-        #encode
-        data=requests.get(data)
-        #print data
-        #keyfrom=pythonxiaodeng&doctype=json&q=student&version=1.1&key=929705525&type=data
-        url=self.url+'?'+data               #链接url和参数dict
-        html=requests.get(url).read()
-        html=json.loads(html)
-        return html
+# 翻译函数，word 需要翻译的内容
+def translate(word):
+    # 有道词典 api
+    url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule&smartresult=ugc&sessionFrom=null'
+    # 传输的参数，其中 i 为需要翻译的内容
+    key = {
+        'type': "AUTO",
+        'i': word,
+        "doctype": "json",
+        "version": "2.1",
+        "keyfrom": "fanyi.web",
+        "ue": "UTF-8",
+        "action": "FY_BY_CLICKBUTTON",
+        "typoResult": "true"
+    }
+    # key 这个字典为发送给有道词典服务器的内容
+    response = requests.post(url, data=key)
+    # 判断服务器是否相应成功
+    if response.status_code == 200:
+        # 然后相应的结果
+        return response.text
+    else:
+        print("有道词典调用失败")
+        # 相应失败就返回空
+        return None
 
-#调用
-youdao=Youdao('student')
-result=youdao.getTranslation()
-for key in result:
-    print(key)
+def get_reuslt(repsonse):
+    # 通过 json.loads 把返回的结果加载成 json 格式
+    result = json.loads(repsonse)
+    print("输入的词为：%s" % result['translateResult'][0][0]['src'])
+    # print()
+    print("翻译结果为：%s" % result['translateResult'][0][0]['tgt'])
+
+def main():
+    print("本程序调用有道词典的API进行翻译，可达到以下效果：")
+    print("外文-->中文")
+    print("中文-->英文")
+    word = input('请输入你想要翻译的词或句：')
+    list_trans = translate(word)
+    print(list_transa)
+    get_reuslt(list_trans)
+
+if __name__ == '__main__':
+    main()
